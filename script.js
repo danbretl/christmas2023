fcd1abb0082b01d304191ed1c9ca302bec0bf030
Dan Bretl <danbretl@gmail.com>
2023-12-09 23:34:16 -0800

    Change to a grid of items

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-function addGift() {
-    const nameInput = document.getElementById("nameInput");
-    const urlInput = document.getElementById("urlInput");
-    const giftList = document.getElementById("giftList");
+const items = [
+    { name: "Item 1", imageUrl: "https://via.placeholder.com/300", url: "https://www.example.com/item1" },
+    { name: "Item 2", imageUrl: "https://via.placeholder.com/300", url: "https://www.example.com/item2" },
+    { name: "Item 3", imageUrl: "https://via.placeholder.com/300", url: "https://www.example.com/item3" },
+    // Add more items as needed
+];
 
-    if (nameInput.value !== "" && urlInput.value !== "") {
-        const listItem = document.createElement("li");
-        listItem.innerHTML = `
-            <span>${nameInput.value}</span>
-            <span><a href="${urlInput.value}" target="_blank">View</a></span>
-            <button type="button" onclick="removeGift(this)">Remove</button>
-        `;
-        giftList.appendChild(listItem);
-        nameInput.value = "";
-        urlInput.value = "";
-    }
-}
+const gridContainer = document.getElementById("itemGrid");
 
-function removeGift(button) {
-    const listItem = button.parentNode;
-    const giftList = listItem.parentNode;
-    giftList.removeChild(listItem);
-}
+items.forEach(item => {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid-item");
+
+    const image = document.createElement("img");
+    image.src = item.imageUrl;
+    image.alt = item.name;
+    image.classList.add("item-image");
+
+    const details = document.createElement("div");
+    details.classList.add("item-details");
+
+    const itemName = document.createElement("div");
+    itemName.textContent = item.name;
+    itemName.classList.add("item-name");
+
+    const itemUrl = document.createElement("a");
+    itemUrl.href = item.url;
+    itemUrl.target = "_blank";
+    itemUrl.textContent = "Visit Website";
+    itemUrl.classList.add("item-url");
+
+    details.appendChild(itemName);
+    details.appendChild(itemUrl);
+
+    gridItem.appendChild(image);
+    gridItem.appendChild(details);
+
+    gridContainer.appendChild(gridItem);
+});
